e6229981ac22af58fec99cfaed6e4338d9f059a0
Edwin R. Paredes <erolandopc@gmail.com>
2017-02-23 16:57:03 -0600

    Mostrar resultados e intentos para 2 intentos y 5 posiciones

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require_relative 'lib/juego.rb'
 
 get '/' do
 	@@juego = Juego.new
-	@nPosiciones = 5
+	#@nPosiciones = 5
+	@hideMsg = "hidden"
 	erb :"juego"    
 end
 
@@ -11,12 +12,20 @@ get '/disparar' do
 	
   	posicion = params["p"].to_i
 	disparo= @@juego.disparar(posicion)
+	
 	if (disparo==true)
 		@resultado = "<i class='glyphicon glyphicon-glass'></i> GANASTE"
+		@hideJuego = "hidden"
+		@hideMsg = ""
 	elsif (disparo==false)
 		@resultado="<i class='glyphicon glyphicon-warning-sign'></i> INTENTA DE NUEVO"
+		@hideJuego = ""
+		@hideMsg = "hidden"
 	else
 		@resultado="GAME OVER"
+		@hideJuego = "hidden"
+		@hideMsg = ""
 	end
+	
 	erb :"juego"
 end
