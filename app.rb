564dba33cd763a002969d511dd79f02c535ecd9d
Edwin R. Paredes <erolandopc@gmail.com>
2017-02-24 09:29:55 -0600

    UI: deshabilitar la posición seleccionada y colores en los mensajes de resultado

--- app.rb
+++ app.rb
@@ -3,28 +3,32 @@ require_relative 'lib/juego.rb'
 
 get '/' do
 	@@juego = Juego.new 0
-	@nPosiciones = 5
+	@@nPosiciones = 5
 	@hideMsg = "hidden"
+	@alerta = "default"
 	erb :"juego"    
 end
 
 get '/disparar' do
 	
-  	posicion = params["p"].to_i
-	disparo= @@juego.disparar(posicion)
+  	@posicion = params["p"].to_i
+	disparo= @@juego.disparar(@posicion)
 	
 	if (disparo==true)
 		@resultado = "<i class='glyphicon glyphicon-glass'></i> GANASTE"
 		@hideJuego = "hidden"
 		@hideMsg = ""
+		@alerta = "success"
 	elsif (disparo==false)
 		@resultado="<i class='glyphicon glyphicon-warning-sign'></i> INTENTA DE NUEVO"
 		@hideJuego = ""
 		@hideMsg = "hidden"
+		@alerta = "warning"
 	else
 		@resultado="GAME OVER, POSICION DE EXITO #{ @@juego.posicion_exito }"
 		@hideJuego = "hidden"
 		@hideMsg = ""
+		@alerta = "danger"
 	end
 	
 	erb :"juego"
