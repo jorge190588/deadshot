c2567c47d27c1b0f6ea2879079b18484bc8972c3
jorge190588 <netneill@hotmail.com>
2017-02-24 13:46:20 -0300

    fix en pruebas

--- spec/juego_spec.rb
+++ spec/juego_spec.rb
@@ -1,32 +1,32 @@
 require_relative "../lib/juego.rb"
 
-describe "iniciar juego" do
+describe "iniciar juego en modo un jugador con dos intentos" do
 	it "numero de intentos 0 de 2" do
-		juego = Juego.new 1
+		juego = Juego.new 1,2
 		juego.contador_intentos.should == 0
 	end
 end
 
-describe "disparar" do
+describe "disparar en modo un jugador con dos intentos" do
 
 	it "disparar con valor 0" do
-		juego = Juego.new 1
+		juego = Juego.new 1,2
 		juego.disparar(2).should == false
 	end
 
 	it "disparar con valor 1" do
-		juego = Juego.new 1
+		juego = Juego.new 1,2
 		juego.disparar(1).should == true
 	end
 
 	it "disparar con valor 2" do
-		juego = Juego.new 1
+		juego = Juego.new 1,2
 		juego.disparar(2).should == false
 	end
 
 
 	it "disparar mas de 2 veces" do
-		juego = Juego.new 1
+		juego = Juego.new 1,2
 		juego.disparar(2)
 		juego.disparar(2)
 		juego.disparar(2).should == -1
@@ -34,3 +34,6 @@ describe "disparar" do
 end
 
 
+describe "disparar en modo dos jugadores con 5 intentos" do
+
+end
